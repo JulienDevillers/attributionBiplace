6b8151fdeb12661c151c3e256658ab9e0d8cc8dc
Julien Devillers <qdaemon.fr@gmail.com>
2025-10-21 00:18:07 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import argparse
 import csv
 import sys
-from dataclasses import dataclass
-
 
 # format fichier tab :  pilote  canardos    hardware_priority_1 hardware_priority_2 hardware_priority_3 hardware_priority_4 hardware_priority_5 hardware_priority_6
 
@@ -20,6 +18,7 @@ class Pilot:
         global hardwares
         hardwares.append(hardware)
 
+
 class Attribution:
     def __init__(self, pilot: str, hardware: str):
         self.pilot = pilot
@@ -27,54 +26,65 @@ class Attribution:
         self.satisfied = False
 
 
+class Combination:
+    def __init__(self, attributions: list[Attribution]):
+        self.attributions: list[Attribution] = attributions
+        self.evaluation = -1
+
+    def evaluate(self):
+        bookings = {}
+        satisfiedCount = 0
+
+        for attribution in self.attributions:
+            hardware = attribution.hardware
+            if not hardware in bookings:
+                bookings[hardware] = 1
+                satisfiedCount += 1
+                attribution.satisfied = True
+        self.evaluation = satisfiedCount
+
+    def toStr(self, excludeNotSatisfied: bool) -> str:
+        result = "["
+        for attribution in self.attributions:
+            if attribution.satisfied or excludeNotSatisfied:
+                result += "[" + attribution.pilot + ", " + attribution.hardware + "], "
+        result = result[:-2] + "]"
+        return result
+
+
+class Run:
+    def __init__(self):
+        self.combinations: list[Combination] = []
+
+    def append_combinaison(self, pilots: list[Pilot], pilot_id: int, preinit: list[Attribution]):
+        if pilot_id >= len(pilots):
+            self.combinations.append(Combination(preinit))
+        else:
+            for request in pilots[pilot_id].requests:
+                p = preinit.copy()
+                attribution = Attribution(pilots[pilot_id].name, request)
+                p.append(attribution)
+                self.append_combinaison(pilots, pilot_id + 1, p)
+
+    def init(self, pilots: list[Pilot]):
+        self.append_combinaison(pilots, 0, [])
+
+    def combinaisonsCount(self):
+        return len(self.combinations)
+
+    def at(self, index: int):
+        return self.combinations[index]
+
+
 pilots: list[Pilot] = []
 hardwares: list[str] = []
-
-
-def append_combinaison(combinaisons: list[list[Attribution]], pilots: list[Pilot], pilot_id: int, preinit: list[Attribution]):
-    if pilot_id >= len(pilots):
-        combinaisons.append(preinit)
-    else:
-        for request in pilots[pilot_id].requests:
-            p = preinit.copy()
-            attribution = Attribution(pilots[pilot_id].name, request)
-            p.append(attribution)
-            append_combinaison(combinaisons, pilots, pilot_id + 1, p)
-
-
-def initCombinaisons(combinaisons: list[list[Attribution]], pilots: list[Pilot]):
-    append_combinaison(combinaisons, pilots, 0, [])
-
-
-def evaluate_combinaison(combinaison) -> bool:
-    bookings = {}
-    satisfied = 0
-    for attribution in combinaison:
-
-        hardware = attribution.hardware
-        if not hardware in bookings:
-            bookings[hardware] = 1
-            satisfied += 1
-            attribution.satisfied = True
-
-    return satisfied
-
-
-def combinaisonToStr(combinaison: list[Attribution], excludeNotSatisfied: bool) -> str:
-    result = "["
-    for attribution in combinaison:
-        if attribution.satisfied or excludeNotSatisfied:
-            result += "[" + attribution.pilot + ", " + attribution.hardware + "], "
-    result = result[:-2] + "]"
-    return result
 
 
 def attribution():
     global pilots
     global hardwares
 
-    best_evaluation = 0
-    best_combinaison: list[Attribution]
+    best_combinaison: Combination = None
 
     pilots.sort(key=lambda pilot: pilot.canardos)
 
@@ -84,21 +94,22 @@ def attribution():
         print("\nRun " + str(i + 1) + " pilotes")
 
         selected_pilots = pilots[:i + 1]
-        combinaisons: list[list[Attribution]] = []
 
-        initCombinaisons(combinaisons, selected_pilots)
+        run = Run()
+        run.init(selected_pilots)
+        combinaisonsCount = run.combinaisonsCount()
+        for j in range(0, combinaisonsCount - 1):
+            combination = run.at(j)
+            combination.evaluate()
+            print(combination.toStr(True) + " -> ", str(combination.evaluation))
 
-        for combinaison in combinaisons:
-            evaluation = evaluate_combinaison(combinaison)
-            print(combinaisonToStr(combinaison, True) + " -> ", str(evaluation))
-            if evaluation > best_evaluation:
-                best_evaluation = evaluation
-                best_combinaison = combinaison.copy()
-                found = evaluation == len(hardwares)
+            if best_combinaison is None or combination.evaluation > best_combinaison.evaluation:
+                best_combinaison = combination
+                found = best_combinaison.evaluation == len(hardwares)
         i += 1
 
-    print("\nBest combinaison:")
-    print(combinaisonToStr(best_combinaison, False) + " -> " + str(best_evaluation))
+    print("\nBest combination:")
+    print(best_combinaison.toStr(False) + " -> " + str(best_combinaison.evaluation))
 
 
 def load_data(filename: str):
