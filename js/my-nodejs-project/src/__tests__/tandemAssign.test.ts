507fb247025c7b7b19fa4232f661c663dc5c68dd
Julien Devillers <qdaemon.fr@gmail.com>
2025-11-26 17:14:15 +0100

    Ajout d'un gros test

--- js/my-nodejs-project/src/__tests__/tandemAssign.test.ts
+++ js/my-nodejs-project/src/__tests__/tandemAssign.test.ts
@@ -217,5 +217,55 @@ describe('assignTandemToPilots', () => {
     expect(getAssignmentForPilotName(result.assignments, 'E')?.tandemName).toBe('L');
     expect(getAssignmentForPilotName(result.assignments, 'F')?.tandemName).toBe('M');
   });
+
+
+  it('should solve this set 4', () => {
+    const pilots: Pilot[] = [
+      { name: 'A', points: 1, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'B', points: 2, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'C', points: 3, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'D', points: 4, wishes: ['I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R'] },
+      { name: 'E', points: 5, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R'] },
+      { name: 'F', points: 6, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'G', points: 7, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'H', points: 8, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'I', points: 9, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'J', points: 10, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'K', points: 11, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'L', points: 12, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'M', points: 13, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'N', points: 14, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'O', points: 15, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'P', points: 16, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'Q', points: 17, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+      { name: 'R', points: 18, wishes: ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] },
+    ];
+    const result = assignTandemToPilots(pilots);
+
+    expect(result).toBeDefined();
+    expect(result.assignments).toBeDefined();
+    expect(result.assignments.length).toBe(18);
+    expect(result.assignmentData).toBeDefined();
+    expect(getAssignmentForPilotName(result.assignments, 'A')?.tandemName).toBe('I');
+    expect(getAssignmentForPilotName(result.assignments, 'B')?.tandemName).toBe('J');
+    expect(getAssignmentForPilotName(result.assignments, 'C')?.tandemName).toBe('K');
+    expect(getAssignmentForPilotName(result.assignments, 'D')?.tandemName).toBe('L');
+    expect(getAssignmentForPilotName(result.assignments, 'E')?.tandemName).toBe('M');
+    expect(getAssignmentForPilotName(result.assignments, 'F')?.tandemName).toBe('N');
+    expect(getAssignmentForPilotName(result.assignments, 'G')?.tandemName).toBe('O');
+    expect(getAssignmentForPilotName(result.assignments, 'H')?.tandemName).toBe('P');
+    expect(getAssignmentForPilotName(result.assignments, 'I')?.tandemName).toBe('Q');
+    expect(getAssignmentForPilotName(result.assignments, 'J')?.tandemName).toBe('R');
+    expect(getAssignmentForPilotName(result.assignments, 'K')?.tandemName).toBe(undefined);
+    expect(getAssignmentForPilotName(result.assignments, 'L')?.tandemName).toBe(undefined);
+    expect(getAssignmentForPilotName(result.assignments, 'M')?.tandemName).toBe(undefined);
+    expect(getAssignmentForPilotName(result.assignments, 'N')?.tandemName).toBe(undefined);
+    expect(getAssignmentForPilotName(result.assignments, 'O')?.tandemName).toBe(undefined);
+    expect(getAssignmentForPilotName(result.assignments, 'P')?.tandemName).toBe(undefined);
+    expect(getAssignmentForPilotName(result.assignments, 'Q')?.tandemName).toBe(undefined);
+    expect(getAssignmentForPilotName(result.assignments, 'R')?.tandemName).toBe(undefined);
+  });
+
+
 });
 
