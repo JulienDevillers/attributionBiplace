dcda9ca31acc3d8e88c0fdd254e1b8c3cf8ba0ef
Julien Devillers <julien@juliendevillers.com>
2025-12-04 10:43:53 +0100

    Fix explication de fonctionnement de l'algo brute force

--- python/tandemAssign.py
+++ python/tandemAssign.py
@@ -2,17 +2,7 @@ from types_ import *
 import time
 
 
-# Principe de calcul :
-# Note : il s'agit d'un algo brute force qui ne fonctionne que parce qu'on a une petite volumétrie.
-# 1- Soit n le nombre de matériels disponibles
-# 2- On choisit les n pilotes les plus prioritaires
-# 3- On crée toutes les combinaisons d'attribution (pilote-matériel) possibles
-# 4- Elles sont triées par priorité du pilote puis par priorité de souhait de matériel
-# 5- La solution retenue est celle qui permet de servir tous les pilotes avec un matériel différent pour chaque
-#    tout en minimisant la priorité du premier pilote non servi.
-# 6- S'il n'existe pas de solution parce qu'il y a des conflits de souhaits de matériels entre les pilotes,
-#     on fait rentrer le pilote suivant en terme de priorité dans le calcul et on recommence en 3
-
+# Class Run, permet d'obtenir toutes les solutions possibles
 class Run:
     # Renvoie un tableau de biplaces. L ieme est affecté au ieme pilote (ordonné du plus au moins prioritaire)
     def get_first_combination(self, pilots: list[Pilot]) -> list[str]:
@@ -71,15 +61,20 @@ def attributions_to_array(attributions: list[str], pilots: list[Pilot]) -> list[
     return result
 
 
-def this_unassigned_is_better_than_this(this_is_better, than):
-    if len(this_is_better) < len(than):
+# Compare deux listes d'id de pilotes non assignés(rappel : les ids représentent la priorité).
+# La meilleure est la plus courte.
+# En cas d'égalité, on compare le contenu. On itère sur les éléments des deux listes tant que les ids
+# de sont les mêmes. Dès qu'ils sont différents, la meilleure solution est celle qui sert le
+# pilote de priorité plus élevée.
+def this_unassigneds_is_better_than_this_one(this, this_one):
+    if len(this) < len(this_one):
         return True
-    if len(this_is_better) > len(than):
+    if len(this) > len(this_one):
         return False
 
     i = 0
-    while i < len(this_is_better):
-        if this_is_better[i] > than[i]:
+    while i < len(this):
+        if this[i] > this_one[i]:
             return True
         i += 1
     return False
@@ -103,16 +98,18 @@ def evaluate(combination: list[str]):
     return len(tandems_assigned), unassigneds
 
 
-# Principe:
-# On travaille sur des itérations à n, puis n+1, puis n+2 pilotes plus prioritaires.
-# On s'arrête dès qu'on a affecté tous les biplaces
-# Au sein de chaque itération, on calcule toutes les possibilités et on retient
-# Celle qui affecte du matériel au plus grand nombre de pilotes et qui minimise
-# la priorité du premier pilote sans biplace ( = pour deux solutions qui affectent
-# le mm nombre de pilotes avec des pilotes sans affectation, on préfère celle qui affecte
-# du matériel au pilote de priorité 2 que de priorité 3.
-# L'ordre dans lequel la partie qui produit les solutions fonctionne a son importance.
-
+# Affecte les biplaces au pilotes
+# Principe de calcul :
+# Note : il s'agit d'un algo brute force qui ne fonctionne que parce qu'on a une petite volumétrie.
+# 1- Soit n le nombre de matériels disponibles
+# 2- On choisit les n pilotes les plus prioritaires
+# 3- On évalue toutes les combinaisons d'attribution (pilote-matériel) possibles et on retient
+#    celle qui affecte du matériel au plus grand nombre de pilotes et qui minimise
+#    les priorités des pilotes sans biplace ( = pour deux solutions qui affectent
+#    le mm nombre de pilotes avec des pilotes sans affectation, on préfère celle qui affecte
+#    du matériel au pilote de priorité 2 que de priorité 3). Voir fonction this_unassigned_is_better_than_this_one.
+#    L'ordre dans lequel la partie qui produit les solutions fonctionne a son importance (voir fonction Run.get_next_combination)
+# 4- S'il existe une solution qui attribue tous les biplaces, l'aglo s'arrête sinon on reprend à l'étape 2 avec n = n+1
 def assign(pilots: list[Pilot]):
     start_ns = time.time_ns()
     best_combination: list[str]
@@ -146,7 +143,7 @@ def assign(pilots: list[Pilot]):
         while run.get_next_combination(selected_pilots, combination):
             count += 1
             evaluation, unassigneds = evaluate(combination)
-            if evaluation > best_evaluation or (evaluation == best_evaluation and this_unassigned_is_better_than_this(unassigneds, best_unassigneds)):
+            if evaluation > best_evaluation or (evaluation == best_evaluation and this_unassigneds_is_better_than_this_one(unassigneds, best_unassigneds)):
                 best_combination = combination.copy()
                 best_evaluation = evaluation
                 best_unassigneds = unassigneds
